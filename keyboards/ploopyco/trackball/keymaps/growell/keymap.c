6411aeabe7607e7c706300f52adcd6a5e2a99047
Garrett Rowell <garrettrowell@gmail.com>
2024-09-25 16:47:42 -0600

    tapdance ish works on windows.

--- keyboards/ploopyco/trackball/keymaps/growell/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/growell/keymap.c
@@ -17,9 +17,20 @@
  */
 #include QMK_KEYBOARD_H
 
+// Tap Dance declarations
+enum {
+    TD_SCROLL_WHEEL,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for KC_BTN3, twice for DRAG_SCROLL
+    [TD_SCROLL_WHEEL] = ACTION_TAP_DANCE_DOUBLE(KC_BTN3, DRAG_SCROLL),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( /* Base */
-        KC_BTN1, DRAG_SCROLL, KC_BTN4,
-          KC_BTN5, KC_BTN2
+        KC_BTN1, TD(TD_SCROLL_WHEEL), KC_BTN5,
+          KC_BTN4, KC_BTN2
     ),
 };
